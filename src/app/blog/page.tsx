1e040b6767d48f86e345f1160f274c6426681967
Maxi Vecchio <maximilianovecchio@gmail.com>
2024-10-03 12:24:14 -0300

    update

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,145 +1,183 @@
-"use client";
+'use client';
 
-import React, { useState, useEffect } from "react";
-import { useWordpress } from "@/context/wordpress-context";
-import { useAuth } from "@/context/platform-user-context";
-import { useSearchParams, useRouter } from "next/navigation"; 
-import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+import React, { useState, useEffect } from 'react';
+import { useWordpress } from '@/context/wordpress-context';
+import { useAuth } from '@/context/platform-user-context';
+import { useSearchParams, useRouter } from 'next/navigation';
+import { Tabs, TabsList, TabsTrigger, TabsContent } from '@/components/ui/tabs';
 import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table";
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table';
 
-import { Badge } from "@/components/ui/badge"
+import { Badge } from '@/components/ui/badge';
 
 import MoveUpLoader from '@/components/shared/moveup-loader';
+import { Button } from '@/components/ui/button';
+
+import { FiRefreshCw } from 'react-icons/fi';
 
 const BlogPage = () => {
-    const { fetchPosts } = useWordpress();
-    const { completeUser } = useAuth();
-    const searchParams = useSearchParams();
-    const router = useRouter();
+  const { fetchPosts } = useWordpress();
+  const { completeUser } = useAuth();
+  const searchParams = useSearchParams();
+  const router = useRouter();
 
-    const [postsByStatus, setPostsByStatus] = useState({
-        publish: [],
-        future: [],
-        private: [],
-        draft: [],
-        trash: [],
-    });
+  const [postsByStatus, setPostsByStatus] = useState({
+    publish: [],
+    future: [],
+    private: [],
+    draft: [],
+    trash: [],
+  });
 
-    const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const statusFromUrl = searchParams.get("status") || "publish";
+  const statusFromUrl = searchParams.get('status') || 'publish';
 
-    const [selectedStatus, setSelectedStatus] = useState(statusFromUrl);
+  const [selectedStatus, setSelectedStatus] = useState(statusFromUrl);
 
-    const postStatus = [
-        { name: "Publicado", slug: "publish", light: {color: '#012609', bg: '#deffe5'}, dark: {color: '#deffe5', bg: '#012609'} },
-        { name: "Programados", slug: "future", light: {color: '#055b69', bg: '#e0f7fa'}, dark: {color: '#e0f7fa', bg: '#055b69'} }, 
-        { name: "Privados", slug: "private", light: {color: '#2a2d30', bg: '#d8e2ed'}, dark: {color: '#d8e2ed', bg: '#2a2d30'} }, 
-        { name: "Borrador", slug: "draft", light: {color: '#3d2f03', bg: '#ffecb5' }, dark: {color: '#ffecb5', bg: '#3d2f03'} }, 
-        { name: "Papelera", slug: "trash", light: {color: '#dc3545', bg: '#f8d7da'}, dark: {color: '#dc3545', bg: '#f8d7da'}  },
-      ];
-      
-    const getPostStatusBySlug = (slug) => {
-        const status = postStatus.find((status) => status.slug === slug);
-        return status || null; 
-      };
+  const postStatus = [
+    {
+      name: 'Publicado',
+      slug: 'publish',
+      light: { color: '#012609', bg: '#deffe5' },
+      dark: { color: '#deffe5', bg: '#012609' },
+    },
+    {
+      name: 'Programados',
+      slug: 'future',
+      light: { color: '#055b69', bg: '#e0f7fa' },
+      dark: { color: '#e0f7fa', bg: '#055b69' },
+    },
+    {
+      name: 'Privados',
+      slug: 'private',
+      light: { color: '#2a2d30', bg: '#d8e2ed' },
+      dark: { color: '#d8e2ed', bg: '#2a2d30' },
+    },
+    {
+      name: 'Borrador',
+      slug: 'draft',
+      light: { color: '#3d2f03', bg: '#ffecb5' },
+      dark: { color: '#ffecb5', bg: '#3d2f03' },
+    },
+    {
+      name: 'Papelera',
+      slug: 'trash',
+      light: { color: '#dc3545', bg: '#f8d7da' },
+      dark: { color: '#dc3545', bg: '#f8d7da' },
+    },
+  ];
 
-    const loadPosts = async () => {
-        setIsLoading(true);
-        try {
-            const posts = await fetchPosts({ status: selectedStatus });
-            /* setTimeout(() => { */
-                setPostsByStatus((prevPostsByStatus) => ({
-                    ...prevPostsByStatus,
-                    [selectedStatus]: posts,
-                }));
-            /* }, 2500); */
-        } catch (error) {
-            console.error("Error fetching posts:", error);
-        } finally {
-            /* setTimeout(() => { */
-                setIsLoading(false);
-              /* }, 2500); */
-        }
-    };
+  const getPostStatusBySlug = (slug) => {
+    const status = postStatus.find((status) => status.slug === slug);
+    return status || null;
+  };
 
-    useEffect(() => {
-        if (!completeUser) return;
-        loadPosts();
-    }, [completeUser, selectedStatus, fetchPosts]);
+  const loadPosts = async () => {
+    setIsLoading(true);
+    try {
+      const posts = await fetchPosts({ status: selectedStatus });
+      /* setTimeout(() => { */
+      setPostsByStatus((prevPostsByStatus) => ({
+        ...prevPostsByStatus,
+        [selectedStatus]: posts,
+      }));
+      /* }, 2500); */
+    } catch (error) {
+      console.error('Error fetching posts:', error);
+    } finally {
+      /* setTimeout(() => { */
+      setIsLoading(false);
+      /* }, 2500); */
+    }
+  };
 
-    useEffect(() => {
-        router.push(`?status=${selectedStatus}`);
-    }, [selectedStatus]);
+  useEffect(() => {
+    if (!completeUser) return;
+    loadPosts();
+  }, [completeUser, selectedStatus, fetchPosts]);
 
-    return (
-        <div className="p-4 max-w-6xl mx-auto">
-            <h1 className="text-xl font-semibold mb-4">Administrar entradas</h1>
+  useEffect(() => {
+    router.push(`?status=${selectedStatus}`);
+  }, [selectedStatus]);
 
-            <Tabs
-                defaultValue={selectedStatus}
-                onValueChange={(value) => setSelectedStatus(value)}
-            >
-                <TabsList className="mb-4">
-                    {postStatus.map((status) => (
-                        <TabsTrigger key={status.slug} value={status.slug}>
-                            {status.name}
-                        </TabsTrigger>
-                    ))}
-                </TabsList>
+  return (
+    <div className='p-4 max-w-6xl mx-auto'>
+      <h1 className='text-xl font-semibold mb-4'>Administrar entradas</h1>
 
-                {postStatus.map((status) => (
-                    <TabsContent key={status.slug} value={status.slug}>
-                        <Table>
-                            {/* <TableCaption>Lista de posts en el estado: {status.name}</TableCaption> */}
-                            <TableHeader>
-                                <TableRow>
-                                    <TableHead>Título</TableHead>
-                                    <TableHead className="w-32">Estado</TableHead>
-                                    <TableHead className="w-32">Fecha</TableHead>
-                                    <TableHead className="w-32"></TableHead>
-                                </TableRow>
-                            </TableHeader>
-                            <TableBody>
-                                {isLoading && postsByStatus[selectedStatus].length === 0 ? (
-                                    <TableRow>
-                                        <TableCell colSpan={4} className="text-center">
-                                            <div className="min-h-48 grid place-items-center"><MoveUpLoader/></div>
-                                        </TableCell>
-                                    </TableRow>
-                                ) : (
-                                    postsByStatus[selectedStatus].map((post, index) => (
-                                        <TableRow key={index}>
-                                            <TableCell className="font-medium">{post.title.rendered}</TableCell>
-                                            <TableCell>
-                                            <Badge
-                                                className={``}
-                                                style={{backgroundColor: getPostStatusBySlug(post.status)?.light.bg, color: getPostStatusBySlug(post.status)?.light.color}}
-                                                variant="outline"
-                                                >
-                                                {getPostStatusBySlug(post.status)?.name}
-                                            </Badge>
-                                            </TableCell>
-                                            <TableCell>{new Date(post.date).toLocaleDateString()}</TableCell>
-                                            <TableCell>bot1 bpt2</TableCell>
-                                        </TableRow>
-                                    ))
-                                )}
-                            </TableBody>
-                        </Table>
-                    </TabsContent>
-                ))}
-            </Tabs>
+      <Tabs
+        defaultValue={selectedStatus}
+        onValueChange={(value) => setSelectedStatus(value)}
+      >
+        <div className='w-full flex justify-between items-center'>
+
+          <TabsList className='mb-4'>
+            {postStatus.map((status) => (
+              <TabsTrigger key={status.slug} value={status.slug}>
+                {status.name}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+          <Button disabled={isLoading} onClick={loadPosts} variant='outline'>
+            {isLoading && <FiRefreshCw className={'animate-spin mr-1.5'} />}
+            Actualizar
+          </Button>
         </div>
-    );
+
+        {postStatus.map((status) => (
+          <TabsContent key={status.slug} value={status.slug}>
+            <Table>
+              {/* <TableCaption>Lista de posts en el estado: {status.name}</TableCaption> */}
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Título</TableHead>
+                  <TableHead className='w-32'>Estado</TableHead>
+                  <TableHead className='w-32'>Fecha</TableHead>
+                  <TableHead className='w-32'></TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {isLoading && postsByStatus[selectedStatus].length === 0 ? (
+                  <TableRow>
+                    <TableCell colSpan={4} className='text-center'>
+                      <div className='min-h-48 grid place-items-center'><MoveUpLoader /></div>
+                    </TableCell>
+                  </TableRow>
+                ) : (
+                  postsByStatus[selectedStatus].map((post, index) => (
+                    <TableRow key={index}>
+                      <TableCell className='font-medium'>{post.title.rendered}</TableCell>
+                      <TableCell>
+                        <Badge
+                          className={``}
+                          style={{
+                            backgroundColor: getPostStatusBySlug(post.status)?.light.bg,
+                            color: getPostStatusBySlug(post.status)?.light.color,
+                          }}
+                          variant='outline'
+                        >
+                          {getPostStatusBySlug(post.status)?.name}
+                        </Badge>
+                      </TableCell>
+                      <TableCell>{new Date(post.date).toLocaleDateString()}</TableCell>
+                      <TableCell>bot1 bpt2</TableCell>
+                    </TableRow>
+                  ))
+                )}
+              </TableBody>
+            </Table>
+          </TabsContent>
+        ))}
+      </Tabs>
+    </div>
+  );
 };
 
 export default BlogPage;
