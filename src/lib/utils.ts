157d51a1ba58a249024c06b3a30d0a7eb204f267
Leonardo Decanini <leodede04@gmail.com>
2024-10-07 21:34:52 -0300

    asd

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,9 +7,9 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-const directionServer = true;
+const directionServer = false;
 
-const directionHome = true;
+const directionHome = false;
 
 export const ServerUrl = directionServer
   ? 'http://localhost:3000'
