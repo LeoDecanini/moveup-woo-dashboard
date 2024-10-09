563d53ec00021c47e02fb3bf16b9916376133aec
Maxi Vecchio <maximilianovecchio@gmail.com>
2024-10-09 01:29:34 -0300

    SCROLL FIXED

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <head />
         <body
           className={cn(
-            'min-h-screen bg-background font-sans antialiased',
+            'min-h-screen bg-background font-sans antialiased overflow-hidden',
             '[&_.slate-selected]:!bg-primary/20 [&_.slate-selection-area]:border [&_.slate-selection-area]:border-primary [&_.slate-selection-area]:bg-primary/10',
             fontSans.variable
           )}
